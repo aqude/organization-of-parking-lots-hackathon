37e18bb16ec388184f8c1a7185c64a116713eb07
Andrei <andrei.hudalla@outlook.com>
2023-11-23 21:30:13 -0600

    Added client template

---
+++ client/app.config.ts
@@ -0,0 +1,9 @@
+export default defineAppConfig({
+    ui: {
+        primary: 'emerald',
+        gray: 'neutral',
+        icons: {
+            dynamic: true
+        }
+    }
+})
---
+++ client/layouts/default.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <header class="__header"></header>
+  <main class="__main">
+    <slot />
+  </main>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
