79b678ce45c6b57f1efb9bc65d0d2e674800f554
aqude <aquadefin@gmail.com>
2023-11-25 17:03:57 +0300

    add icon

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -4,11 +4,21 @@
 
 <template>
     <div class="__home">
+      <nuxt-link to="/profile">
+        <Icon name="solar:user-bold" class="iconClient"/>
+      </nuxt-link>
         <ClientLocationInfo />
         <ClientGoogleMaps />
     </div>
 </template>
 
-<style lang="scss" scoped>
-
+<style scoped>
+.iconClient {
+  position: absolute;
+  width: 70px;
+  height: 70px;
+  padding: 10px;
+  z-index: 2;
+  right: 0;
+}
 </style>
