c6e5801b750f2401dfdbf9aaa804ef98e5adc909
aqude <aquadefin@gmail.com>
2023-11-25 22:23:06 +0300

    fix docker by backenders

--- backend/docker_scripts/app_run.sh
+++ backend/docker_scripts/app_run.sh
@@ -1,12 +1,6 @@
-#!/bin/bash
-
-sh -c './wait-for db:5432 -- npm start'  # doesnt work nc command is missing!
-
-sleep 30  # wait for bd
-
-cd app/db
+#!/bin/bash 
+sleep 15  # wait for bd
+cd app/db 
 alembic upgrade head
-
-cd ../../
-
-gunicorn app.__main__:app --workers 2 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
+cd ../../ 
+gunicorn app.__main__:app --workers 2 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000 
--- backend/docker_scripts/celery_run.sh
+++ backend/docker_scripts/celery_run.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-sh -c './wait-for db:5432 -- npm start'  # doesnt work nc command is missing!
-
 sleep 30  # wait for bd
-
 celery -A app.worker.celery_app worker -l INFO --concurrency=2 -B
