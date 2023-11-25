302d46b7c53ca32842b24e77161b885765f636a1
aqude <aquadefin@gmail.com>
2023-11-26 01:08:39 +0300

    обертка с хеадерами и прочее

--- client/composables/useAPI.ts
+++ client/composables/useAPI.ts
@@ -1,5 +1,14 @@
 type ArgumentTypes<F extends Function> = F extends (...args: infer A) => any ? A : never;
 
-export const useAPI = (request: ArgumentTypes<typeof useFetch>[0], opts: ArgumentTypes<typeof useFetch>[1]) => {
-	return useFetch(`http://localhost:8000${request}`, opts)
-}
+export const useAPI = (request: ArgumentTypes<typeof useFetch>[0], opts: ArgumentTypes<typeof useFetch>[1] & { headers?: Record<string, string> }) => {
+	const customHeaders = {
+		"Content-Type": "application/x-www-form-urlencoded",
+	};
+
+	const mergedOpts = {
+		...opts,
+		headers: { ...opts.headers, ...customHeaders },
+	};
+
+	return useFetch(`http://localhost:80${request}`, mergedOpts);
+};
