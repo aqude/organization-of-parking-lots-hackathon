6554591da021af1c6179439395672af9d0ed0e8b
aqude <aquadefin@gmail.com>
2023-11-25 23:01:28 +0300

    test vue

--- client/pages/profile.vue
+++ client/pages/profile.vue
@@ -3,9 +3,16 @@
 </script>
 
 <template>
-
+  <div class="__profile">
+    <div class="flex flex-row justify-between text-xl pt-10">
+      <h1 class="">Регистрация</h1>
+     <Icon name="settings"/>
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss">
-
+.__profile {
+  align-items: center;
+}
 </style>
